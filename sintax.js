4c333214b46e99bc2c77120367fa2eb77ec98e04
José Arthur Lermen <110497031+JoseLermen@users.noreply.github.com>
2022-11-14 10:16:18 -0300

    Update sintax.js

--- sintax.js
+++ sintax.js
@@ -1,17 +1,17 @@
-
-
-document.getElementById("botao").onclick = function() {generator()};
-
+document.getElementById("botao").onclick = function () {
+  generator();
+};
 
 function generator() {
-    const number = Math.floor(Math.random() * 10);
-        if (number > 3){
-            document.getElementById("texto").innerHTML = '“Just one small positive thought in the morning can change your whole day.”'
-        }
-        else if (number > 6){
-            document.getElementById("texto").innerHTML =  '“Opportunities don`t happen, you create them.”'
-        }
-        else {
-            document.getElementById("texto").innerHTML =  '“Love your family, work super hard, live your passion.”'
-        }
+  const number = Math.floor(Math.random() * 10);
+  if (number > 3) {
+    document.getElementById("texto").innerHTML =
+      "“Just one small positive thought in the morning can change your whole day.”";
+  } else if (number > 6) {
+    document.getElementById("texto").innerHTML =
+      "“Opportunities don`t happen, you create them.”";
+  } else {
+    document.getElementById("texto").innerHTML =
+      "“Love your family, work super hard, live your passion.”";
+  }
 }
